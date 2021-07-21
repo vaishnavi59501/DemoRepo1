a93bce0013a7d8c51a4cdd7b10d1232a663a6539
vaishnavi59501 <84762136+vaishnavi59501@users.noreply.github.com>
2021-07-21 18:16:40 +0530

    committed change

--- loopscript.sh
+++ loopscript.sh
@@ -2,11 +2,11 @@
 #shell variables
 index=1
 #using while loop to print 1 to 15 integers
-#while [ $index -le 15 ]
-#do
-  #echo $index
- # index=$(($index + 1))
-#done
+while [ $index -le 15 ]
+do
+  echo $index
+  index=$(($index + 1))
+done
 
 #using for loop to print 1 to 15 integers
 for ((i=1;i<=15;i++))
