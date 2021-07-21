50a870a29d6c4bfb7d69f83204e942413c45a1f0
vaishnavi59501 <84762136+vaishnavi59501@users.noreply.github.com>
2021-07-21 16:28:39 +0530

    added new file script

---
+++ conditionscript.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#finding bigger number from the passed arguments using if condition
+
+if [ $1 -gt $2 ]
+then
+    echo $1 "is greater"
+else
+    echo $2 "is greater"
+fi
+
+echo the script name is $0
+echo PID of script is $$
+echo total arguments count is $#
+echo last return code $?
+echo all arguments $*
