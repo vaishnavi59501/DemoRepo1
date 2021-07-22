ecd9e0c926448d804247487f574450acf9519bde
vaishnavi59501 <84762136+vaishnavi59501@users.noreply.github.com>
2021-07-22 17:23:55 +0530

    added new script file

---
+++ sedscript.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#shell variables
+path=/home/devopsadmin/sample.txt
+#replacing single word in a line"
+echo "I am tom" | sed 's/tom/john/'
+sed 's/work/play/' $path
+echo "-------------------"
+#replacing all the occurrences of matched word
+sed 's/study/learn/g' $path
+echo "-------------------"
+#deleting line if containing the particular word
+sed '/while/d' $path
+echo "-------------------"
+#deleting a particular word color from a line
+sed '3s/color//g' $path
+echo "-------------------"
+#dispalying specific column value
+sed '4s/[^:]* \([^:]* \)[^:]*/\1/' $path
