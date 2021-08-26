ea18c6d181c1e5873a84531247d46f8256e5090d
vaishnavi59501 <84762136+vaishnavi59501@users.noreply.github.com>
2021-08-26 19:38:22 +0530

    added python script

--- demo_script.py
+++ demo_script.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-
+#this script makes use of different loops such as while, for loop and if... else decision making statement
 yes=input("enter 1 or 0: ")
 #if...else
 if int(yes):
@@ -12,6 +12,7 @@ else:
 
 a=20
 b=20
+#checks whether two variables points to same object
 if(a is b):
 	print("a is identity to b")
 else:
@@ -19,14 +20,16 @@ else:
 
 
 li=[56,57,6,8,69]
-
+#checks whether the given element is member of list
 if(56 in li):
 	print("it is a member of list")
 else:
 	print("it is not a member of list")
+	
 #while loop
 tot=0
 i=0
+#iterates through list for adding the elements in list
 while(i<len(li)):
 	tot+=li[i]
 	i+=1
@@ -34,11 +37,13 @@ print(tot)
 
 #for loop
 list1=[ ]
+#converting each element in given list to complex data type
 for i in li:
 	list1.append(complex(i,1))
 
 print(list1)
 
+#block of code which tells whether given number is prime or not
 num=int(input("enter a number"))
 
 if(num%2==0):
